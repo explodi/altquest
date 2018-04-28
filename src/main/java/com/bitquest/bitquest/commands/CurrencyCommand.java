bca2db290345d9f7cd366900a818f397e6965068
AltQuest <altquest09@gmail.com>
2018-04-27 21:27:37 -0400

    Add files via upload
    updated to use BitQuest.DENOMINATION_NAME for REDIS "currency"

--- src/main/java/com/bitquest/bitquest/commands/CurrencyCommand.java
+++ src/main/java/com/bitquest/bitquest/commands/CurrencyCommand.java
@@ -29,8 +29,8 @@ public class CurrencyCommand extends CommandAction {
 			player.sendMessage(ChatColor.GREEN+"Currency changed to " + BitQuest.REDIS.get("currency"+player.getUniqueId().toString()));                        
 
 				}
-			else if((args[0].equalsIgnoreCase("btc"))||(args[0].equalsIgnoreCase("bitcoin"))) {
-				BitQuest.REDIS.set("currency"+player.getUniqueId().toString(), "bitcoin");
+			else if((args[0].equalsIgnoreCase("btc"))||(args[0].equalsIgnoreCase("bitcoin"))||(args[0].equalsIgnoreCase(BitQuest.DENOMINATION_NAME))) {
+				BitQuest.REDIS.set("currency"+player.getUniqueId().toString(), BitQuest.DENOMINATION_NAME);
 				player.sendMessage(ChatColor.GREEN+"Currency changed to " + BitQuest.REDIS.get("currency"+player.getUniqueId().toString()));
 	                               						   
 			} else {
